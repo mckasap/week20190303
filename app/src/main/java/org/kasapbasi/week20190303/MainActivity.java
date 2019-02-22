a3d8694f27f1fba45eb0bf29d9797107b73d1db8
mckasap <mckasap@gmail.com>
2019-02-22 16:22:57 +0300

    İlk Gönderim

--- app/src/main/java/org/kasapbasi/week20190303/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week20190303/MainActivity.java
@@ -1,13 +1,19 @@
 package org.kasapbasi.week20190303;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListAdapter( new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.ulkeler))
+                      );
     }
 }
