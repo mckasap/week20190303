46efeda2b9ddaab0a7192e4298cf8f2ddf163bba
mckasap <mckasap@gmail.com>
2019-02-22 16:48:26 +0300

    İlk Gönderim

--- app/src/main/java/org/kasapbasi/week20190303/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week20190303/MainActivity.java
@@ -1,9 +1,13 @@
 package org.kasapbasi.week20190303;
 
+import android.app.LauncherActivity;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
@@ -15,5 +19,14 @@ public class MainActivity extends ListActivity {
                 android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.ulkeler))
                       );
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Toast.makeText(this, l.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show();
+
     }
 }
